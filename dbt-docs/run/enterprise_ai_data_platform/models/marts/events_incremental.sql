3f1b0fbeac066188aa43d9acf1d1017a000f13fd
justin-mbca <justin-mbca@users.noreply.github.com>
2026-06-20 08:57:14 +0000

    deploy: e133ee317916965edb5cf85bdad6980817796b35

--- dbt-docs/run/enterprise_ai_data_platform/models/marts/events_incremental.sql
+++ dbt-docs/run/enterprise_ai_data_platform/models/marts/events_incremental.sql
@@ -4,7 +4,7 @@
             where (
                 event_id) in (
                 select (event_id)
-                from "events_incremental__dbt_tmp20260619102638313904"
+                from "events_incremental__dbt_tmp20260620085707154397"
             );
 
         
@@ -13,6 +13,6 @@
     insert into "data"."main_marts"."events_incremental" ("event_id", "event_type", "related_entity_id", "entity_type", "event_metadata", "event_timestamp", "event_date", "event_hour")
     (
         select "event_id", "event_type", "related_entity_id", "entity_type", "event_metadata", "event_timestamp", "event_date", "event_hour"
-        from "events_incremental__dbt_tmp20260619102638313904"
+        from "events_incremental__dbt_tmp20260620085707154397"
     )
   
